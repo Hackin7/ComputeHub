7f208396648ce81207bb77580a3e889c599e1739
Hackin7 <zunmun@gmail.com>
2016-04-02 23:46:25 +0800

    4/2/2016

--- Software/OpenHAB/OpenHABUI.py
+++ Software/OpenHAB/OpenHABUI.py
@@ -17,20 +17,24 @@ class UIConfig:
       def func():  
         def add():
             if mode == 0:
-                name = UIConfig().add()
+                name = UIConfig().addlayout()
                 if name != '': layout.floors.append(name)
                 UIConfig().floors()
             elif mode == 1:
-                name = UIConfig().add()
+                name = UIConfig().addlayout()
                 if name != '': layout.rooms.append((details[0],name))
                 UIConfig().rooms(details[0])()
+            elif mode == 2:
+                name = UIConfig().addlayout()
+                if name != '': layout.devices.append((details[0],details[1],name))
+                UIConfig().devices(details[0],details[1])()
             MenuUI.back = 1
-        def change(): pass
+        def change(): UIConfig().selection(mode,details)
         make_button('+', 165, 30, 27, 30, 5, yellow, 24, add)
-        make_button('-', 205, 30, 27, 30, 5, yellow, 24, add)
-        make_button('<>', 245, 30, 27, 50, 5, blue, 24, change)
+        make_button('Select', 205, 30, 27, 90, 5, blue, 24, change)
+        #make_button('<>', 245, 30, 27, 50, 5, blue, 24, change)
       return func
-    def add(self):
+    def addlayout(self):
         global name
         name = ''
         def change():
@@ -67,6 +71,88 @@ class UIConfig:
           if MenuUI.back == 1:
               MenuUI.back = 0
               return name
+
+    def selection(self,mode,details):
+        counter = 1
+        things = []
+        if mode == 0:
+                while counter <= len(layout.floors):
+                  thing = layout.floors[counter-1]
+                  thing = (thing,blue,20,UIConfig().select(0,thing))
+                  things.append(thing)
+                  counter = counter + 1
+                MenuUI.menu.load(MenuUI.menu.slotconf,'Select',yellow,things,passs)
+                UIConfig().floors()
+        elif mode == 1:
+                while counter <= len(layout.rooms):
+                  thing = layout.rooms[counter-1]
+                  if thing[0] == details[0]:
+                      thing = (thing[1],blue,20,UIConfig().select(1,thing))
+                      things.append(thing)
+                  counter = counter + 1
+                MenuUI.menu.load(MenuUI.menu.slotconf,'Select',yellow,things,passs)
+                UIConfig().rooms(details[0])()
+        elif mode == 2:
+            while counter <= len(layout.devices):
+              thing = layout.devices[counter-1]
+              if (thing[0] == details[0]) and (thing[1] == details[1]):
+                  thing = (thing[2],blue,20,UIConfig().select(2,thing))
+                  things.append(thing)
+              counter = counter + 1
+              MenuUI.menu.load(MenuUI.menu.slotconf,'Select',yellow,things,passs)
+        MenuUI.back = 1
+            
+    def select(self,mode,details):
+      def func():
+        if mode == 0:
+            level = 'Floor'
+            name = 'name'
+            def remove(floor):
+                def func():
+                    layout.floors.remove(floor)
+                    UIConfig().remove(1,floor)
+                    MenuUI.back = 1
+                return func
+        elif mode == 1:
+            level = 'Room'
+            name = 'name'
+            def remove(room):
+                def func():
+                    layout.rooms.remove(room)
+                    UIConfig().remove(2,room)
+                    MenuUI.back = 1
+                return func
+        elif mode == 2:
+            level = 'Floor'
+            name = 'name'
+        def layout():
+            global name
+            clearall()
+            make_label(level+':', 40, 40, 60, cyan)
+            make_button(name, 40, 100, 40, 240, 5, yellow, 40, change)
+            def exit():
+              global name
+              name = ''
+              MenuUI.back = 1
+      return func
+
+    def remove(self,mode,details):
+        counter = 1
+        if mode == 1:
+            oglist = tuple(layout.rooms)
+            while counter <= len(oglist):
+                thing = oglist[counter-1]
+                if thing[0] == details:
+                    layout.rooms.remove(thing)
+                    UIConfig().remove(2,thing)
+                counter = counter + 1
+        if mode == 2:
+            oglist = tuple(layout.devices)
+            while counter <= len(oglist):
+              thing = oglist[counter-1]
+              if (thing[0] == details[0]) and (thing[1] == details[1]): layout.devices.remove(thing)
+              counter = counter + 1
+              
     def floors(self):
         counter = 1
         things = []
--- Software/OpenHAB/layout.py
+++ Software/OpenHAB/layout.py
@@ -6,4 +6,6 @@ rooms.append(('Floor1', 'Room1'))
 rooms.append(('Floor2', 'Room2'))
 rooms.append(('Floor1', 'Room3'))
 rooms.append(('Floor2', 'Room4'))
+rooms.append(('Floor1', 'Room5'))
 devices = []
+devices.append(('Floor1', 'Room1', 't'))
